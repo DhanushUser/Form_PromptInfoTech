9a9278e674d1d1112d96117b78d3d02bae84828a
Dhanush <159154132+DhanushUser@users.noreply.github.com>
2024-08-22 14:23:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('registrationForm').addEventListener('submit', function (e) {
+      e.preventDefault();
+      
+      const password = document.getElementById('password').value;
+      const confirmPassword = document.getElementById('confirmPassword').value;
+    
+      if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+      } else {
+        alert("Registration successful!");
+        
+      }
+    });
+  });
+  
